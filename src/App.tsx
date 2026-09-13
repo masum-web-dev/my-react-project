74bdd0e3ed26d54831ef81e7b71d169b39898dc5
masum697 <masum.akash1999@gmail.com>
2026-09-13 16:45:33 +0630

    Initial commit: Project setup with Vite, Tailwind, and DaisyUI

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1 className='text-3xl font-bold underline text-amber-700'>Hello, Vite + React!</h1>
+    </>
+  )
+}
+
+export default App
